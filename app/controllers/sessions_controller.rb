a89a15ac8d97a65c5166bdd863e7ea2062cce3c6
Knoxx04 <onyiknoxx3@gmail.com>
2023-03-27 15:39:22 +0300

    complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
-    def create
+    def login
         user = User.find_by(username: params[:username])
         session[:user_id] = user.id
         render json: user
       end
 
-      def destroy
+      def logout
         session.delete :user_id
         head :no_content
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+  post 'login', to: 'sessions#login'
+  delete 'logout', to: 'sessions#logout'
+  get "/me", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-  get "/me", to: "users#show"
-end
+#   post "/login", to: "sessions#create"
+#   delete "/logout", to: "sessions#destroy"
+#   get "/me", to: "users#show"
+ end
